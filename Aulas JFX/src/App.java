5890438f2d83c255be323ae04404ec911e7aeb06
BIELZnX <92862029+BIELZnX@users.noreply.github.com>
2022-06-03 14:20:25 -0300

    Adicionando os arquivos

---
+++ Aulas JFX/src/App.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    @Override
+    public void start(Stage tela) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ProjetoBiblioteca.fxml"));
+        Scene cena = new Scene(root);
+        tela.setScene(cena);
+        tela.show(); 
+    }
+
+}
