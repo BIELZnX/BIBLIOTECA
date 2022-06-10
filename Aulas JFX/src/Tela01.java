3b2129c4d9a99385c8da878331bab39520cce796
RailaAlves <106748873+RailaAlves@users.noreply.github.com>
2022-06-10 15:19:32 -0300

    Adicionando código primeira tela

---
+++ Aulas JFX/src/Tela01.java
@@ -0,0 +1,31 @@
+package Visual;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+public class Tela01 {
+
+    @FXML
+    private ScrollPane Recente;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private TextField pesquisa;
+
+    @FXML
+    private ScrollPane recomendados;
+
+}
